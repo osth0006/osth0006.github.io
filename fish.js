befa0e635ecff6980efbac459662ef4832d2d08f
Osthus, Peter <posthus@lmi.org>
2020-05-21 08:40:17 -0400

    minor change

--- fish.js
+++ fish.js
@@ -442,7 +442,7 @@ Fish.prototype = {
 		// draw the fish on the canvas
 		//ctx.lineWidth = 2;
 		ctx.fillStyle = this.color;
-		//ctx.strokeStyle = this.color;
+		ctx.lineStyle = "black";
 		ctx.beginPath();
 		ctx.moveTo(x1, y1);
 		ctx.quadraticCurveTo(x2,y2,x,y);
